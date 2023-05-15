d65a16202e40c01334b589a078a220b64095cabc
Harriet H-W <harriethw@gmail.com>
2023-05-15 15:54:01 +0100

    add shared types from API
    we will autogenerate these eventually.

---
+++ server/@types/shared/index.d.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type { ApprovedPremisesUser } from './models/ApprovedPremisesUser';
+export type { ApprovedPremisesApplication } from './models/ApprovedPremisesApplication';
+export type { ArrayOfOASysRiskOfSeriousHarmSummaryQuestions } from './models/ArrayOfOASysRiskOfSeriousHarmSummaryQuestions';
+export type { OASysQuestion } from './models/OASysQuestion';
+export type { OASysSection } from './models/OASysSection';
+export type { OASysSections } from './models/OASysSections';
+export type { OASysSupportingInformationQuestion } from './models/OASysSupportingInformationQuestion';
+export type { Person } from './models/Person';
+export type { User } from './models/User';
+export type { UserQualification } from './models/UserQualification';
+export type { UserRole } from './models/UserRole';
