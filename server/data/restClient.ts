c685094c73186ed4e0d56568a95cac7e3227d021
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-07-19 15:07:29 +0100

    Lint: RestClient

--- server/data/restClient.ts
+++ server/data/restClient.ts
@@ -39,11 +39,7 @@ export default class RestClient {
 
   defaultHeaders: Record<string, string>
 
-  constructor(
-    private readonly name: string,
-    private readonly config: ApiConfig,
-    private readonly token: string,
-  ) {
+  constructor(private readonly name: string, private readonly config: ApiConfig, private readonly token: string) {
     this.agent = config.url.startsWith('https') ? new HttpsAgent(config.agent) : new Agent(config.agent)
     this.defaultHeaders = { 'X-SERVICE-NAME': 'cas2' }
   }
